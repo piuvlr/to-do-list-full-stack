1853a82dc5e0455e2ba244e9dc12f4baf9f59b0b
Davi Nicolino <101908992+davinico@users.noreply.github.com>
2022-10-07 15:51:02 -0300

    Delete back/tasks/doc directory

--- back/tasks/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"br.com.caio.todo.tasks.utils"}];updateSearchResults();
--- back/tasks/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.caio.todo.tasks.utils","l":"ParseUtils"}];updateSearchResults();
