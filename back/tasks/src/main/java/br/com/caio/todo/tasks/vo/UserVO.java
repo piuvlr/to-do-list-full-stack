71efe6597892571b1d1ac9184691e9830e97904e
Piuavlr <63988968+piuvlr@users.noreply.github.com>
2022-11-11 23:13:48 -0300

    feito

--- back/tasks/src/main/java/br/com/caio/todo/tasks/vo/UserVO.java
+++ back/tasks/src/main/java/br/com/caio/todo/tasks/vo/UserVO.java
@@ -2,16 +2,22 @@ package br.com.caio.todo.tasks.vo;
 
 import java.util.Date;
 
+import br.com.caio.todo.tasks.status.EmailPermissionsUserEnum;
+
 public class UserVO {
 	private Integer id;
 	private String userName;
 	private Date creationDate;
 	private String password;
+	private String emailUser;
+	private EmailPermissionsUserEnum emailPermissionsUserEnum;
 
-	public UserVO(Integer id, String userName, Date creationDate) {
+	public UserVO(Integer id, String userName, Date creationDate, String emailUser, EmailPermissionsUserEnum emailPermissionsUserEnum) {
 		this.id = id;
 		this.userName = userName;
 		this.creationDate = creationDate;
+		this.emailUser = emailUser;
+		this.emailPermissionsUserEnum = emailPermissionsUserEnum;
 	}
 
 	public Integer getId() {
@@ -40,6 +46,22 @@ public class UserVO {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public String getEmailUser() {
+		return emailUser;
+	}
+
+	public void setEmailUser(String emailUser) {
+		this.emailUser = emailUser;
+	}
+
+	public EmailPermissionsUserEnum getEmailPermissionsUserEnum() {
+		return emailPermissionsUserEnum;
+	}
+
+	public void setEmailPermissionsUserEnum(EmailPermissionsUserEnum emailPermissionsUserEnum) {
+		this.emailPermissionsUserEnum = emailPermissionsUserEnum;
 	}
 
 }
